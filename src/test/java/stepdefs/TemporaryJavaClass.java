b27a2b41f831e417a5a38511fb9155f926a738e2
Saravanan C <saravananc242@gmail.com>
2021-05-13 11:10:46 +0530

    latest changes

---
+++ src/test/java/stepdefs/TemporaryJavaClass.java
@@ -0,0 +1,12 @@
+package stepdefs;
+
+public class TemporaryJavaClass {
+
+	public static void main(String[] args) {
+		testMethod();
+	}
+
+	public static  void testMethod() {
+		System.out.println("Test");
+	}
+}
